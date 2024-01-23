3ff39e64c39c8997e637361873ccbc5bceea0c5d
Netochukwu Nnakwe <68166363+netonnakwe@users.noreply.github.com>
2024-01-23 20:34:29 +0100

    project controller crud operations

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -1,21 +1,89 @@
 const Project = require("../models/project.model");
 
-module.exports.getProjects = function(req, res){
-
+module.exports.listProjects = function(req, res){
+    
 };
 
-module.exports.getProject = function(req, res){
+module.exports.getProject = async function(req, res){
+    try {
+        const id = req.project.id
+        const project = await Project.findById(id).select("-password");
+        if(!project) {
+            return res.status(404).json({
+                success: false,
+                message: "Project Not Found"
+            });
+        };
 
+        res.status(200).json({
+            success: true,
+            project: project
+        });
+
+    } catch (error) {
+        consloe.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Failed To Fetch Project Data"
+        });
+    }
 };
 
-module.exports.createProject = function(req, res){
-
+module.exports.createProject = async function(req, res){
+    
+    
 };
 
-module.exports.updateProject = function(req, res){
+module.exports.updateProject = async function(req, res){
+    try {
+        const id = req.project.id
+        const project = await Project.findById(id).select("-password");
+        if(!project) {
+            return res.status(404).json({
+                success: false,
+                message: "Project Not Found"
+            });
+        };
 
+        if (req.project.description !== '') {
+            project.description = req.project.description
+        }
+
+        res.status(200).json({
+            success: true,
+            project: project
+        });
+
+    } catch (error) {
+        consloe.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Failed To Fetch Project Data"
+        });
+    }
 };
 
-module.exports.deleteProject = function(req, res){
-
+module.exports.deleteProject = async function(req, res){
+    try {
+        const id = req.Project.id;
+        const project = await Project.findById(id);
+        if(!project) {
+            return res.status(404).json({
+                success: false,
+                message: "Project Not Found"
+            });
+        };
+        const deletedProject = await Project.findByIdAndDelete(id);
+        res.status(200).json({
+            success: true,
+            message: "Project Deleted Successfully",
+            project: deletedProject,
+          });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          success: false,
+          message: "Could Not Delete Project Data",
+        });
+    };
 };
--- src/routes/project.route.js
+++ src/routes/project.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const projectRouter = express.Router();
 const { getProjects, getProject, createProject, updateProject, deleteProject } = require("../controllers/project.controller");
 
-projectRouter.get("/", getProjects);
+projectRouter.get("/", listProjects);
 projectRouter.get("/:id", getProject);
 projectRouter.post("/", createProject);
 projectRouter.patch("/:id", updateProject);
