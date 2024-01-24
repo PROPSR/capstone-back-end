be490b6e00791a84da75da869c1b8de1767e3c41
Nne1509 <onwukannenna77@gmail.com>
2024-01-24 18:31:16 +0100

    edited route

--- src/routes/supplier.route.js
+++ src/routes/supplier.route.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const supplierRouter = express.Router();
-const { signup, login } = require("../controllers/auth/supplier.auth.controller");
+const { signup } = require("../controllers/auth/supplier.auth.controller");
 const { getSupplier, updateSupplier, uploadProfilePicture, deleteSupplier } = require("../controllers/supplier.controller");
 const { validateSupplierSignup, validateLogin, validateSupplierUpdate } = require("../middlewares/validation");
 const { verifyToken } = require("../middlewares/jwt");
 const { upload } = require("../config/multer")
 
-const { signup } = require("../controllers/auth/supplier.auth.controller");
-const { getSupplier, updateSupplier } = require("../controllers/supplier.controller");
-const { validateSupplierSignup, validateLogin, validateSupplierUpdate } = require("../middlewares/validation")
+
+
 
 supplierRouter.post("/signup", validateSupplierSignup, signup);
-supplierRouter.post("/login", validateLogin, login);
 supplierRouter.get("/", verifyToken("Supplier"), getSupplier);
 supplierRouter.patch("/update",verifyToken("Supplier"), validateSupplierUpdate, updateSupplier);
 supplierRouter.patch("/upload", verifyToken("Supplier"), upload("supplier").single("profilePicture"), uploadProfilePicture);
