79cc423d8299617f8e398939228eaac1679c2b98
Suigeneris <akinfolarindele@gmail.com>
2024-01-26 14:52:59 +0100

    project update

--- src/routes/project.route.js
+++ src/routes/project.route.js
@@ -3,10 +3,12 @@ const projectRouter = express.Router();
 const {verifyToken} = require("../middlewares/jwt");
 const {validateProjectCreation} = require("../middlewares/validation");
 const { listProjects, getProject, projectsMarketplace, createProject} = require("../controllers/project.controller");
+const { upload } = require("../config/multer");
+
 
 projectRouter.get("/", listProjects);
 projectRouter.get("/:id", getProject);
 projectRouter.get("/marketplace", projectsMarketplace);
-projectRouter.post("/", verifyToken("Homeowner"), validateProjectCreation, createProject);
+projectRouter.post("/", verifyToken("Homeowner"), upload("Homeowner").array("images", 10), validateProjectCreation, createProject);
 
 module.exports = projectRouter;
