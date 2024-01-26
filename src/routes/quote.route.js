34ad00dcd299d5c7f647a951047ade3b393bd76b
Nne1509 <onwukannenna77@gmail.com>
2024-01-26 09:24:47 +0100

    worked on quote logic

--- src/routes/quote.route.js
+++ src/routes/quote.route.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const quoteRouter = express.Router();
-const { getQuotes, getQuote, createQuote, updateQuote, deleteQuote } = require("../controllers/quote.controller");
+const { getQuotes, getQuote, getContractorQuotes, getContractorQuote, createQuote, updateQuote, deleteQuote } = require("../controllers/quote.controller");
+const { validateQuoteCreation, validateQuoteUpdate } = require("../middlewares/validation");
+const { verifyToken } = require("../middlewares/jwt");
 
 
 quoteRouter.get("/", getQuotes);
 quoteRouter.get("/:id", getQuote);
-quoteRouter.post("/", createQuote);
-quoteRouter.patch("/:id", updateQuote);
-quoteRouter.delete("/:id", deleteQuote);
+quoteRouter.get("/quotes", verifyToken("Contractor"), getContractorQuotes);
+quoteRouter.get("/quotes/:id", verifyToken("Contractor"), getContractorQuote);
+quoteRouter.post("/", verifyToken("Contractor"), validateQuoteCreation, createQuote);
+quoteRouter.patch("/quotes/:id", verifyToken("Contractor"), validateQuoteUpdate, updateQuote);
+quoteRouter.delete("/quotes/:id", verifyToken("Contractor"), deleteQuote);
 
 module.exports = quoteRouter;
