3315fb77b8fb2ff8bec93cad1363e57fd39ed7ff
Ben Godman <124352547+Leefranklyn@users.noreply.github.com>
2024-02-12 13:56:32 +0100

    Update otp.route.js

--- src/routes/otp.route.js
+++ src/routes/otp.route.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const { verifyEmailVerificationOtp, verifyPasswordResetOtp, resendEmailVerificationOtp, resendPasswordResetOtp } = require("../controllers/auth/otpl.auth.controller");
 const otpRouter = Router();
 
-otpRouter.post("/verifyemail/:id", verifyEmailVerificationOtp);
+otpRouter.post("/verifyemail/", verifyEmailVerificationOtp);
 otpRouter.post("/verifyemail/resend/:id", resendEmailVerificationOtp);
 otpRouter.post("/verifypasswordreset/:id", verifyPasswordResetOtp);
 otpRouter.post("/verifypasswordreset/resend/:id", resendPasswordResetOtp);
