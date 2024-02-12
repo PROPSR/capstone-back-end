7a5ce5ac479db2840321c0f2b6070652e97da05c
Ben Godman <124352547+Leefranklyn@users.noreply.github.com>
2024-02-12 14:12:24 +0100

    Update homeowner.model.js

--- src/models/homeowner.model.js
+++ src/models/homeowner.model.js
@@ -67,7 +67,7 @@ const homeOwnerSchema = new Schema({
 });
 
 homeOwnerSchema.pre("save", async function(){
-    const salt = await bcrypt.genSalt();
+    const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt)
 });
 
